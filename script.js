3a4924d9060d5afe7e9e8f0c90112d61f2a3a679
Murali R <muraliram2002@gmail.com>
2023-08-28 19:52:05 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate content loading
+    setTimeout(function () {
+      document.querySelector(".loading-container").style.display = "none";
+      document.querySelector(".content").style.display = "block";
+    }, 2000); // Adjust the time as needed
+  });
